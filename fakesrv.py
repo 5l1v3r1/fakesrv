03a0ff64bf9321b7d3bad240e189d0252bb86dae
cheddar <jmg2967@rit.edu>
2014-09-04 14:59:51 -0400

    Initial commit

---
+++ fakesrv.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import socket
+import signal
+
+def handleUDP(cs, MSG):
+    (data, addr) = cs.recvfrom(1024)
+    print("%s\n" % data)
+    cs.sendto("%s\n" % MSG, addr)
+
+def handleTCP(cs, addr, MSG):
+    print("Connection from: %s", addr)
+    data = cs.recv(1024)
+    print("%s\n" % data)
+    cs.sendall("%s\n" % MSG)
+
+def main():
+    if len(sys.argv) != 4:
+        print('Usage: %s PORT MSG PROTO', sys.argv[0])
+        sys.exit(1)
+
+    PROTO = sys.argv[1]
+    PORT = int(sys.argv[2])
+    MSG = sys.argv[3]
+
+    signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+
+    if PROTO.upper() == 'TCP':
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    elif PROTO.upper() == 'UDP':
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    else:
+        print('PROTO is either TCP or UDP')
+        sys.exit(1)
+
+    s.bind(('0.0.0.0', PORT))
+
+    if PROTO.upper() == 'TCP':
+        s.listen(100)
+        while 1:
+            (cs, addr) = s.accept()
+            pid = os.fork()
+
+            if pid == 0:
+                s.close()
+                handleTCP(cs, addr, MSG)
+                sys.exit(0)
+
+            cs.close()
+
+    if PROTO.upper() == 'UDP':
+        while 1:
+            handleUDP(s, MSG)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
